7f5534895a0db313abc30114daa45506c1655bb2
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2025-05-06 12:47:33 +1000

    Improve doc strings and comments for config_updater.py

--- src/experiment_generator/config_updater.py
+++ src/experiment_generator/config_updater.py
@@ -1,7 +1,7 @@
 """
 Config.yaml Updater for the experiment management.
 
-This module provides utilities to update YAML configuration files for experiments.
+This module provides utilities to update `config.yaml` for experiments.
 It includes methods for modifying parameters in both control and perturbation experiments.
 """
 
@@ -12,19 +12,30 @@ from .utils import read_yaml, write_yaml, update_config_entries
 
 class ConfigUpdater:
     """
-    A utility class for updating config.yaml.
+    A utility class for updating `config.yaml`.
     """
 
     def __init__(self, directory: Path) -> None:
+        """
+        Initialise the ConfigUpdater with a working directory.
+        """
         self.directory = directory
 
     def update_config_params(self, param_dict: dict, target_file: Path) -> None:
         """
-        Updates namelist parameters and overwrites namelist file.
+        Update `config.yaml` parameters using values from the input dictionary.
+
+        - Ensures the 'jobname' matches the directory name for consistency.
+        - Overwrites the target YAML file in-place.
+
+        Args:
+            param_dict (dict): Dictionary of parameters to update in config.yaml.
+            target_file (Path): Relative path to the config.yaml file within the directory.
         """
         nml_path = self.directory / target_file
         file_read = read_yaml(nml_path.as_posix())
 
+        # Enforce jobname consistency
         if "jobname" in param_dict:
             if param_dict["jobname"] != self.directory.name:
                 warnings.warn(
@@ -34,6 +45,9 @@ class ConfigUpdater:
                     UserWarning,
                 )
         param_dict["jobname"] = self.directory.name
+
+        # Apply updates to the config.yaml file
         update_config_entries(file_read, param_dict)
 
+        # write to the config.yaml file
         write_yaml(file_read, nml_path.as_posix())
