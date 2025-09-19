5a7e51a128c855ad93440d0268e31bc030a5343b
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2025-09-19 12:16:07 +1000

    Add a new marker for keeping unchanged params - RESERVE

--- src/experiment_generator/common_var.py
+++ src/experiment_generator/common_var.py
@@ -1,6 +1,7 @@
 # Experiment specific attributes
 BRANCH_KEY = "branches"
 REMOVED = "REMOVE"
+RESERVED = "RESERVE"
 
 
 def _is_removed_str(x) -> bool:
@@ -10,3 +11,12 @@ def _is_removed_str(x) -> bool:
     Returns True if `x` is a string equal to REMOVED, otherwise False.
     """
     return isinstance(x, str) and x == REMOVED
+
+
+def _is_reserved_str(x):
+    """
+    Check if a value is the explicit keep marker ("RESERVED").
+
+    Returns True if `x` is a string equal to RESERVED, otherwise False.
+    """
+    return isinstance(x, str) and x == RESERVED
