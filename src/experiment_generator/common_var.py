9cf86335595ac2c819978b57f542b485e395e939
minghangli-uni <24727729+minghangli-uni@users.noreply.github.com>
2025-09-19 12:31:21 +1000

    Add _is_seq helper func

--- src/experiment_generator/common_var.py
+++ src/experiment_generator/common_var.py
@@ -1,3 +1,5 @@
+from collections.abc import Sequence
+
 # Experiment specific attributes
 BRANCH_KEY = "branches"
 REMOVED = "REMOVE"
@@ -13,10 +15,19 @@ def _is_removed_str(x) -> bool:
     return isinstance(x, str) and x == REMOVED
 
 
-def _is_reserved_str(x):
+def _is_reserved_str(x) -> bool:
     """
     Check if a value is the explicit keep marker ("RESERVED").
 
     Returns True if `x` is a string equal to RESERVED, otherwise False.
     """
     return isinstance(x, str) and x == RESERVED
+
+
+def _is_seq(x) -> bool:
+    """
+    Check if a value is a sequence (e.g. list) but not a string.
+
+    Returns True if `x` is a sequence and not a string, otherwise False.
+    """
+    return isinstance(x, Sequence) and not isinstance(x, str)
